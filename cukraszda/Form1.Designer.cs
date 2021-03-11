ceb31f3d9aaa15ba9fae0c8a069f8d470bd48083
FabianTamas <fabiant0130@gmail.com>
2021-03-11 12:50:53 +0100

    Add project files.

---
+++ cukraszda/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace cukraszda
+{
+    partial class frmFo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBeolvas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBeolvas
+            // 
+            this.btnBeolvas.Location = new System.Drawing.Point(13, 13);
+            this.btnBeolvas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBeolvas.Name = "btnBeolvas";
+            this.btnBeolvas.Size = new System.Drawing.Size(105, 31);
+            this.btnBeolvas.TabIndex = 0;
+            this.btnBeolvas.Text = "Beolvasás";
+            this.btnBeolvas.UseVisualStyleBackColor = true;
+            this.btnBeolvas.Click += new System.EventHandler(this.btnBeolvas_Click);
+            // 
+            // frmFo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.btnBeolvas);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmFo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cukrászda";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBeolvas;
+    }
+}
+
---
+++ cukraszda/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cukraszda
+{
+    public partial class frmFo : Form
+    {
+        public frmFo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBeolvas_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
