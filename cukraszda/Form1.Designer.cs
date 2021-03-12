7f55b422dade8cc142c74e9fc1608a8f73794bf9
FabianTamas <fabiant0130@gmail.com>
2021-03-12 08:17:43 +0100

    2. feladat megoldva

--- cukraszda/Form1.Designer.cs
+++ cukraszda/Form1.Designer.cs
@@ -30,12 +30,20 @@ namespace cukraszda
         private void InitializeComponent()
         {
             this.btnBeolvas = new System.Windows.Forms.Button();
+            this.btnElso = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.lbKiiras = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // btnBeolvas
             // 
-            this.btnBeolvas.Location = new System.Drawing.Point(28, 24);
-            this.btnBeolvas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBeolvas.Location = new System.Drawing.Point(13, 30);
+            this.btnBeolvas.Margin = new System.Windows.Forms.Padding(4);
             this.btnBeolvas.Name = "btnBeolvas";
             this.btnBeolvas.Size = new System.Drawing.Size(105, 31);
             this.btnBeolvas.TabIndex = 0;
@@ -43,14 +51,95 @@ namespace cukraszda
             this.btnBeolvas.UseVisualStyleBackColor = true;
             this.btnBeolvas.Click += new System.EventHandler(this.btnBeolvas_Click);
             // 
+            // btnElso
+            // 
+            this.btnElso.Location = new System.Drawing.Point(13, 68);
+            this.btnElso.Name = "btnElso";
+            this.btnElso.Size = new System.Drawing.Size(105, 31);
+            this.btnElso.TabIndex = 1;
+            this.btnElso.Text = "1. feladat";
+            this.btnElso.UseVisualStyleBackColor = true;
+            this.btnElso.Click += new System.EventHandler(this.btnElso_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 142);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 31);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 179);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 31);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 216);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(105, 31);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(13, 253);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(105, 31);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(13, 290);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(105, 31);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // lbKiiras
+            // 
+            this.lbKiiras.FormattingEnabled = true;
+            this.lbKiiras.ItemHeight = 16;
+            this.lbKiiras.Location = new System.Drawing.Point(195, 35);
+            this.lbKiiras.Name = "lbKiiras";
+            this.lbKiiras.Size = new System.Drawing.Size(358, 276);
+            this.lbKiiras.TabIndex = 8;
+            // 
             // frmFo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.lbKiiras);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnElso);
             this.Controls.Add(this.btnBeolvas);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmFo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cukrászda";
@@ -61,6 +150,14 @@ namespace cukraszda
         #endregion
 
         private System.Windows.Forms.Button btnBeolvas;
+        private System.Windows.Forms.Button btnElso;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ListBox lbKiiras;
     }
 }
 
--- cukraszda/Form1.cs
+++ cukraszda/Form1.cs
@@ -29,6 +29,22 @@ namespace cukraszda
                 cukis.Add(new Cuki(a[0], a[1], bool.Parse(a[2]), int.Parse(a[3]), a[4]));
             }
             be.Close();
+
+            MessageBox.Show("Beolvasva.");
+        }
+
+        private void btnElso_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(0, cukis.Count);
+            lbKiiras.Items.Clear();
+            for (int i = 0; i < cukis.Count; i++)
+            {
+                if (i == index)
+                {
+                    lbKiiras.Items.Add(cukis[index].Nev);
+                }
+            }
         }
     }
 }
